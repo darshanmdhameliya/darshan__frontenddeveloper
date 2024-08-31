6df74885d7b9a450040a23bb85e41726548d2c8f
darshandhameliya000 <skillqodedarshan@gmail.com>
2024-08-31 18:06:41 +0530

    30-08-2024

---
+++ 7.reactJS/lecture6 nextul prectice/nextui/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import Model from './componets/Model';
+import Navbar1 from './componets/Navbar';
+
+function App() {
+  return (
+    // <Model />
+    <Navbar1/>
+  );
+}
+
+export default App;
