56cb9b0010fb881309d499ef6a7b68158f92c752
darshandhameliya000 <skillqodedarshan@gmail.com>
2025-01-30 10:13:07 +0530

    30-01-2025 Localstorage , Session , Cookie store value

---
+++ Frontend/9.react/lecture24 LocalStorage/script.js
@@ -0,0 +1,39 @@
+
+// function setdata() {
+//     localStorage.setItem("username", "darshan")
+//     localStorage.setItem("age", "20")
+// }
+// function getdata() {
+//     let data = localStorage.getItem("username")
+//     document.getElementById("text").innerHTML = data
+// }
+// function removedata() {
+//     localStorage.removeItem("username")
+//     //    document.getElementById("text").innerHTML=data
+// }  
+// function cleardata() {
+//     localStorage.clear()
+//     //    document.getElementById("text").innerHTML=data
+// }  
+
+
+// function setdata() {
+//     sessionStorage.setItem("username", "darshan")
+//     sessionStorage.setItem("age", "20")
+// }
+// function getdata() {
+//     let data = sessionStorage.getItem("age")
+//     document.getElementById("text").innerHTML = data
+// }
+// function removedata() {
+//     sessionStorage.removeItem("username")
+//     //    document.getElementById("text").innerHTML=data
+// }
+// function cleardata() {
+//     sessionStorage.clear()
+//     //    document.getElementById("text").innerHTML=data
+// }
+
+
+document.cookie= "username=darshan"//set cookie
+document.cookie= "username=darshan ; expires=Thu, 30 Jan 2025 05:35:00 UTC";//set cookie time for UTC formate
